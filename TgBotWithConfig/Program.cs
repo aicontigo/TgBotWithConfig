db411ad8ff4729a4335b55d6c51a89f0a69e2377
Alexey Bubley <alexey.bubley@discovercarhire.com>
2025-05-15 00:24:29 +0200

    Create console, add appsettings.json

---
+++ TgBotWithConfig/Program.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.Configuration;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        var config = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory()) 
+            .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+            .Build();
+
+        Console.WriteLine($"Bot's token: {config["Telegram:BotToken"]}");
+        Console.ReadLine();
+    }
+}
