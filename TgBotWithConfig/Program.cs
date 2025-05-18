ffb3b9a1aa9c975ae61bff9163aa02c9c723a4fe
Alex <alex@aicontigo.com>
2025-05-18 21:57:22 +0200

    read one of Development or  Production appsettings

--- TgBotWithConfig/Program.cs
+++ TgBotWithConfig/Program.cs
@@ -4,9 +4,12 @@ internal class Program
 {
     private static void Main(string[] args)
     {
+        var environment = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") ?? "Production";
+
         var config = new ConfigurationBuilder()
-            .SetBasePath(Directory.GetCurrentDirectory()) 
+            .SetBasePath(Directory.GetCurrentDirectory())
             .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+            .AddJsonFile($"appsettings.{environment}.json", optional: false, reloadOnChange: true)
             .Build();
 
         Console.WriteLine($"Bot's token: {config["Telegram:BotToken"]}");
